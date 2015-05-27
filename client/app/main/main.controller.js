2634db4f8cba626a7ab470663ae427b1607f3f8b
vonquilon <von.quilon@gmail.com>
2015-05-26 23:30:36 -0400

    first commit

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('bazooApp')
+  .controller('MainCtrl', function ($scope, $http, socket) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+      socket.syncUpdates('thing', $scope.awesomeThings);
+    });
+
+    $scope.addThing = function(user) {
+      if($scope.newThing === '') {
+        return;
+      }
+      $http.post('/api/things', { name: user, info: $scope.newThing });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('thing');
+    });
+  });
